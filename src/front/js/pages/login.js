e56ab57a15432a478741cdde22bcf5a1a5b92aab
lutacch <lu.tacchetti@gmail.com>
2023-01-09 18:18:14 +0000

    login

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,47 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+
+import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  console.log("this is your token", store.token);
+
+  const handleClick = () => {
+    if(actions.login(email, password))
+    navigate("/single")
+  
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>login</h1>
+
+      {store.token && store.token != "" && store.token != undefined ? (
+        "You are logged in with this token" + token
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          ></input>
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          ></input>
+          <button onClick={handleClick}>Login</button>{" "}
+        </div>
+      )}
+    </div>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,102 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      syncTokenFromSessionStore: () => {
+        const token = sessionStorage.getItem("token");
+        console.log("Application just loaded");
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
+      logout: () => {
+        const token = sessionStorage.removeItem("token");
+        console.log("Login out");
+        setStore({ token: null });
+      },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+      getMessage: async () => {
+		const store = getStore();
+		const opts = {
+			headers: {
+				Authorization: "Bearer " + store.token
 			}
 		}
-	};
+        try {
+          // fetching data from the backend
+          const resp = await fetch("https://3001-lutacch-login-afvorbimoxv.ws-eu81.gitpod.io/api/hello", opts);
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+        try {
+          const resp = await fetch(
+            "https://3001-lutacch-login-afvorbimoxv.ws-eu81.gitpod.io/api/token",
+            opts
+          );
+
+          if (resp.status !== 200) {
+            alert("there has been some error");
+            return false;
+          }
+          const data = await resp.json();
+          console.log("this came from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("there hast been an error login");
+        }
+      },
+    },
+  };
 };
 
 export default getState;
