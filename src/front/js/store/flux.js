cad84bfe3d50a899969de7a10b2a74a61e33ab32
lutacch <lu.tacchetti@gmail.com>
2023-01-10 11:37:19 +0000

    commit

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -29,7 +29,7 @@ const getState = ({ getStore,  setStore }) => {
 		}
         try {
           // fetching data from the backend
-          const resp = await fetch("https://3001-lutacch-login-afvorbimoxv.ws-eu81.gitpod.io/api/hello", opts);
+          const resp = await fetch(process.env.BACKEND_URL +"/api/hello", opts);
           const data = await resp.json();
           setStore({ message: data.message });
           // don't forget to return something, that is how the async resolves
@@ -51,7 +51,7 @@ const getState = ({ getStore,  setStore }) => {
         };
         try {
           const resp = await fetch(
-            "https://3001-lutacch-login-afvorbimoxv.ws-eu81.gitpod.io/api/token",
+            process.env.BACKEND_URL + "/api/token",
             opts
           );
 
